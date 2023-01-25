f38172f3c1dd1f5809b27c0136aca8696c21abd9
AsparuhB <akbamboff@gmail.com>
2023-01-25 10:19:05 +0200

    Update 5_special_numbers.py

--- Programing_course/2_fundamentals_january_2023/2_Data Types and Variables/1_lectures/5_special_numbers.py
+++ Programing_course/2_fundamentals_january_2023/2_Data Types and Variables/1_lectures/5_special_numbers.py
@@ -5,12 +5,13 @@ number = int(input())
 for num in range(1, number + 1):
     sum_of_digits = 0
     digits = num
-
+    is_true = False
     while digits > 0:
         sum_of_digits += digits % 10
-        digits = int(digits / 10)
+        digits = digits // 10
 
     if (sum_of_digits == 5) or (sum_of_digits == 7) or (sum_of_digits == 11):
-        print(f"{num} -> True")
+        is_true = True
+        print(f"{num} -> {is_true}")
     else:
-        print(f"{num} -> False")
+        print(f"{num} -> {is_true}")
