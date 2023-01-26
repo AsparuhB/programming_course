27df99cb3cebf4bf3b0e201dde433169da8484b5
AsparuhB <akbamboff@gmail.com>
2023-01-26 12:17:19 +0200

    Changes

--- Programing_course/2_fundamentals_january_2023/3_Lists Basics/1_lectures/1_strange_zoo.py
+++ Programing_course/2_fundamentals_january_2023/3_Lists Basics/1_lectures/1_strange_zoo.py
@@ -2,6 +2,10 @@ tail = input()
 body = input()
 head = input()
 
-normal_order = [head, body, tail]
+# normal_order = [head, body, tail]
+# print(normal_order)
 
-print(normal_order)
+order = [tail, body, head]
+order[0], order[2] = order[2], order[0]
+
+print(order)
---
+++ Programing_course/2_fundamentals_january_2023/3_Lists Basics/1_lectures/2_courses.py
@@ -0,0 +1,9 @@
+courses_number = int(input())
+
+list_number = []
+for _ in range(courses_number):
+    course_name = input()
+
+    list_number.append(course_name)
+
+print(list_number)
