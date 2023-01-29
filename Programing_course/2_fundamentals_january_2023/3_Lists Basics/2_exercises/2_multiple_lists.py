fcd15919777f33da0dbfd5de85edc44a989c7c2e
AsparuhB <akbamboff@gmail.com>
2023-01-29 22:55:27 +0200

    Some updates

--- Programing_course/2_fundamentals_january_2023/3_Lists Basics/2_exercises/2_multiple_lists.py
+++ Programing_course/2_fundamentals_january_2023/3_Lists Basics/2_exercises/2_multiple_lists.py
@@ -1,15 +1,22 @@
 factor = int(input())
 count = int(input())
 
+# needed_numbers = []
+# iterations = 0
+#
+# while True:
+#     iterations += 1
+#     if iterations % factor == 0:
+#         needed_numbers.append(iterations)
+#
+#     if iterations >= factor * count:
+#         break
+#
+# print(needed_numbers)
+
 needed_numbers = []
-iterations = 0
 
-while True:
-    iterations += 1
-    if iterations % factor == 0:
-        needed_numbers.append(iterations)
-
-    if iterations >= factor * count:
-        break
+for num in range(1, count + 1):
+    needed_numbers.append(num * factor)
 
 print(needed_numbers)
