0c560c085bc93f5f7cb3a0e5da5313241b4b58ec
AsparuhB <akbamboff@gmail.com>
2023-01-29 21:42:24 +0200

    Update 10_gladiator_expenses.py

--- Programing_course/2_fundamentals_january_2023/2_Data Types and Variables/2_exercises/10_gladiator_expenses.py
+++ Programing_course/2_fundamentals_january_2023/2_Data Types and Variables/2_exercises/10_gladiator_expenses.py
@@ -1,2 +1,25 @@
 duels_lost = int(input())
+helmet_price = float(input())
+sword_price = float(input())
+shield_price = float(input())
+armor_price = float(input())
+
+times_shield_broken = 0
+needed_aureus = 0
+
+for duel in range(1, duels_lost + 1):
+    if duel % 2 == 0:
+        needed_aureus += helmet_price
+
+    if duel % 3 == 0:
+        needed_aureus += sword_price
+
+    if duel % 2 == 0 and duel % 3 == 0:
+        needed_aureus += shield_price
+        times_shield_broken += 1
+        if times_shield_broken % 2 == 0:
+            needed_aureus += armor_price
+
+
+print(f"Gladiator expenses: {needed_aureus:.2f} aureus")
 
