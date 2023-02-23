3c75114fd89017d43f552705c1f2c38e3a626a9c
AsparuhB <akbamboff@gmail.com>
2023-02-23 10:57:34 +0200

    Some changes.

--- Programing_course/2_fundamentals_january_2023/3_Lists Basics/1_lectures/1_strange_zoo.py
+++ Programing_course/2_fundamentals_january_2023/3_Lists Basics/1_lectures/1_strange_zoo.py
@@ -1,11 +1,12 @@
-tail = input()
-body = input()
-head = input()
+order = []
 
 # normal_order = [head, body, tail]
 # print(normal_order)
 
-order = [tail, body, head]
+for _ in range(3):
+    data = input()
+    order.append(data)
+
 order[0], order[2] = order[2], order[0]
 
 print(order)
